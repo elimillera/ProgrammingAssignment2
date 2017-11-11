b65b4ac3430f9c3e306170858024795db84af6ba
elimillera <elimillera@gmail.com>
2017-11-10 20:55:45 -0600

    Project

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a object that cashes the inverse of a matrix once it is solved
+## After matrix is solved once, it should no longer need to call solve() again.
 
-## Write a short comment describing this function
 
+## Creates matrix object from x and has methods to 1)set matrix, this will reset
+## the inverse back to null. 2) Get the inverse, this will return null if it has
+## not been calculated yet. 3) Set the inverse, this is used by the casheSolve()
+## function once the matrix is solved. 4) Get the matrix, for getting the
+## underlying data.
 makeCacheMatrix <- function(x = matrix()) {
+  matrixInverse <- NULL
 
+  set <- function(mat){
+    x <<- mat
+    matrixInverse <<- NULL
+  }
+  getInverse <- function(){
+    matrixInverse
+  }
+  setInverse <- function(mat){
+    matrixInverse <<- mat
+  }
+  get <- function(){
+    x
+  }
 }
 
 
-## Write a short comment describing this function
-
+## Solves the matrix, unless it has already been solved, it would then return
+## the previously solved inverse.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        matInverse <- x$getInverse()
+
+        if(!is.null(matInverse)){
+          return(matInverse)
+        }
+
+        mat <- x$get()
+        matInverse <- solve(mat, ...)
+        x$setInverse(matInverse)
+        return(mat)
 }
